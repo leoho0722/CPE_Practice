1f696d73c0007acefb8958af57ecc050d9258f5c
leoho0722 <leo160918@gmail.com>
2022-02-24 14:13:52 +0800

    UVA10041

--- UVA10041/UVA10041/main.c
+++ UVA10041/UVA10041/main.c
@@ -17,7 +17,7 @@ int main(int argc, const char * argv[]) {
     int member; // 該筆測資的成員數量
     int street[500]; // 成員居住的街道位置
     int medium; // 街道的中位數
-    int sum = 0; // 距離總和
+    int sum; // 距離總和
     
     scanf("%d", &totalCase); // 輸入總共的測資筆數
     printf("總共的測資筆數：%d\n", totalCase);
@@ -31,6 +31,7 @@ int main(int argc, const char * argv[]) {
         // qsort(要排序的陣列, 有幾筆資料, 每筆資料的大小, 比較函數)，要 #include <stdlib.h> 才能用
         qsort(street, member, sizeof(int), comp); // 將每個成員居住的街道位置由小到大排序
         medium = street[member/2]; // 尋找成員居住街道的中位數
+        sum = 0;
         for (int k = 0; k < member; k++) {
             sum += abs(street[k] - medium); // 計算每個成員與中位數的距離總和
         }
